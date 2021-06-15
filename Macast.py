bc45f95566a509aee3ae52790a15a94f6ede265a
xfangfang <2553041586@qq.com>
2021-06-15 17:24:45 +0800

    Change menu bar

--- Macast.py
+++ Macast.py
@@ -4,9 +4,12 @@ import os
 import re
 import time
 import rumps
+import json
+import requests
 import gettext
 import cherrypy
 import logging
+import subprocess
 import threading
 from cherrypy.process.plugins import Monitor
 from utils import loadXML, XMLPath, PORT, NAME, Setting, SYSTEM, SYSTEM_VERSION
@@ -14,7 +17,7 @@ from plugin import SSDPPlugin, MPVPlugin
 
 
 logger = logging.getLogger()
-logger.setLevel(logging.ERROR)
+logger.setLevel(logging.DEBUG)
 
 try:
     locale = Setting.getLocale()
@@ -86,9 +89,12 @@ def notify():
 
 def startCherrypy():
     Setting.load()
-    SSDPPlugin(cherrypy.engine).subscribe()
-    MPVPlugin(cherrypy.engine).subscribe()
-    Monitor(cherrypy.engine, notify, 3).subscribe()
+    ssdpPlugin = SSDPPlugin(cherrypy.engine)
+    ssdpPlugin.subscribe()
+    mpvPlugin = MPVPlugin(cherrypy.engine)
+    mpvPlugin.subscribe()
+    ssdpMonitor = Monitor(cherrypy.engine, notify, 3)
+    ssdpMonitor.subscribe()
     cherrypy_config = {
         'global':{
             'server.socket_host' : '0.0.0.0',
@@ -107,20 +113,77 @@ def startCherrypy():
         }
       }
     cherrypy.quickstart(DLNAHandler(), '/', config = cherrypy_config)
+    ssdpPlugin.unsubscribe()
+    mpvPlugin.unsubscribe()
+    ssdpMonitor.unsubscribe()
+    logger.error("Cherrypy stopped")
 
 class Macast(rumps.App):
     def __init__(self):
-        super(Macast, self).__init__("Macast", "📺", quit_button=None)
-        self.thread = threading.Thread(target=startCherrypy, args=())
-        self.thread.start()
-        # rumps.debug_mode(True)
+        super(Macast, self).__init__("Macast",icon='assets/menu.png', title="", template=True, quit_button=None)
+        self.thread = None
+        self.running = False
+        self.startCast()
+        self.menu = [
+            rumps.MenuItem(_("pause"), callback=self.toggle),
+            None,
+            [
+                rumps.MenuItem(_("Setting")),
+                [
+                    rumps.MenuItem("IP: {}".format(Setting.getIP())),
+                    rumps.MenuItem("Macast (v{})".format(Setting.getVersion())),
+                    rumps.MenuItem(_("Check for updates"), callback=self.checkUpdate),
+                    rumps.MenuItem(_("About"), callback=self.about),
+                ]
+            ]
+        ]
+        rumps.debug_mode(True)
+        threading.Thread(target=self.checkUpdate, kwargs={'verbose': False, 'sender': None}).start()
 
-    @rumps.clicked(_('quit'))
-    def clean_up_before_quit(self, _):
+    def stopCast(self):
         while cherrypy.engine.state != cherrypy.engine.states.STARTED:
             time.sleep(0.5)
         cherrypy.engine.exit()
         self.thread.join()
+        self.running = False
+
+    def startCast(self):
+        if self.running:
+            return
+        self.thread = threading.Thread(target=startCherrypy, args=())
+        self.thread.start()
+        self.running = True
+
+    def checkUpdate(self, sender, verbose=True):
+        try:
+            res = requests.get('https://api.github.com/repos/xfangfang/Macast/releases/latest').text
+            res = json.loads(res)
+            logger.debug("tag_name: {}".format(res['tag_name']))
+            onlineVersion = re.findall(r'(\d+\.*\d+)', res['tag_name'])[0]
+            if float(Setting.getVersion()) < float(onlineVersion):
+                rumps.notification(_("New Update {}").format(res['tag_name']), "", "opening browser")
+                subprocess.run(['open', 'https://github.com/xfangfang/Macast/releases/latest'])
+            else:
+                if verbose:
+                    rumps.alert(_("You're up to date."))
+        except Exception as e:
+            logger.error("get update info error: {}".format(e))
+
+    def about(self, sender):
+        subprocess.run(['open', 'https://github.com/xfangfang/Macast'])
+
+    def toggle(self, sender):
+        if self.running:
+            sender.title = _('start')
+            self.stopCast()
+        else:
+            sender.title = _('pause')
+            self.startCast()
+
+    @rumps.clicked(_('quit'))
+    def clean_up_before_quit(self, _):
+        if self.running:
+            self.stopCast()
         rumps.quit_application()
 
 if __name__ == '__main__':
