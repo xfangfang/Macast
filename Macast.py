f8c08b5c4314e0abe1a6be8fd9a131a6edc7eada
xfangfang <2553041586@qq.com>
2021-09-04 04:02:22 +0800

    Use _MEIPASS in pyinstaller

--- Macast.py
+++ Macast.py
@@ -11,9 +11,16 @@ from macast_renderer.mpv import MPVRenderer
 logger = logging.getLogger("Macast")
 logger.setLevel(logging.DEBUG)
 
+def get_base_path(path="."):
+    if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
+        base_path = sys._MEIPASS
+    else:
+        base_path = os.getcwd()
+    return os.path.join(base_path, path)
+
 try:
     locale = Setting.get_locale()
-    lang = gettext.translation('macast', localedir=os.path.join(os.getcwd(), 'i18n'), languages=[locale])
+    lang = gettext.translation('macast', localedir=get_base_path('i18n'), languages=[locale])
     lang.install()
     logger.error("Macast Loading Language: {}".format(locale))
 except Exception as e:
@@ -24,7 +31,7 @@ except Exception as e:
 if __name__ == '__main__':
     mpv_path = 'mpv'
     if sys.platform == 'darwin':
-        mpv_path = os.path.join(os.getcwd(), 'bin/MacOS/mpv')
+        mpv_path = get_base_path('bin/MacOS/mpv')
     elif sys.platform == 'win32':
-        mpv_path = os.path.join(os.getcwd(), 'bin/mpv.exe')
+        mpv_path = get_base_path('bin/mpv.exe')
     gui(MPVRenderer(_, mpv_path), _)
